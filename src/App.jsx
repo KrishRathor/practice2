2fbdb565dea191cfc1709f2bcf9692c7b9021097
Krish Rathor <krishrathor18@gmail.com>
2025-02-06 11:40:42 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,28 @@ import Form from './components/Form'
 import Header from './components/Header'
 import axios from 'axios'
 import GetData from './components/GetData'
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import About from './pages/About'
 import Profile from './components/Profile'
 import Home from './pages/Home'
 import Details from './pages/Details'
 import Personal from './pages/Personal'
 import Sign from './pages/Sign'
+// hi there
 
 const App = () => {
 
   return (
     <>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>} />
-        <Route path='/details' element={<Details/>} />
-        <Route path='/personal_info' element={<Personal/>} />
-        <Route path='/sign_in' element={<Sign/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/details' element={<Details />} />
+        <Route path='/personal_info' element={<Personal />} />
+        <Route path='/sign_in' element={<Sign />} />
       </Routes>
-      
+
     </>
   )
 }
